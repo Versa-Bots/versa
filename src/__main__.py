51ecce8878665cdd0550c7105badf6ae251fcc49
Paillat-dev <me@paillat.dev>
2026-02-04 23:39:07 +0100

    feat: :construction_worker: Add CI stuff and ruff formatter and linter

--- src/__main__.py
+++ src/__main__.py
@@ -6,18 +6,19 @@ from dotenv import load_dotenv
 bot = discord.Bot(intents=discord.Intents.default())
 
 bot.load_extensions("src.cogs")
-print("Loaded cogs: " + ', '.join(bot.cogs))
+print("Loaded cogs: " + ", ".join(bot.cogs))
 
 
 @bot.listen()
-async def on_connect():
-    print('Connected to Discord!')
+async def on_connect() -> None:
+    print("Connected to Discord!")
 
 
 @bot.listen()
-async def on_ready():
-    print(f'Logged in as {bot.user}')
-    print('------')
+async def on_ready() -> None:
+    print(f"Logged in as {bot.user}")
+    print("------")
+
 
 load_dotenv()
 bot.run(os.getenv("TOKEN"))
--- src/cogs/message_commands.py
+++ src/cogs/message_commands.py
@@ -5,27 +5,27 @@ from discord import message_command
 
 
 class MessageCommands(discord.Cog, name="message_commands"):
-    def __init__(self, bot: discord.Bot):
+    def __init__(self, bot: discord.Bot) -> None:
         self.bot = bot
 
     @message_command()
     async def fxlink(self, ctx: discord.ApplicationContext, message: discord.Message) -> None:
-        """ Makes certain site's links embed properly within Discord """
+        """Makes certain site's links embed properly within Discord."""
         if not message.content:
             await ctx.respond("No link found!", ephemeral=True)
             return
 
-        twitter_pattern = r'(https?://)(www\.)?(twitter\.com|x\.com)'
-        reddit_pattern = r'(https?://)(www\.)?(reddit\.com)'
-        tiktok_pattern = r'(https?://)(www\.)?((vm\.)?tiktok\.com)'
-        instagram_pattern = r'(https?://)(www\.)?(instagram\.com)'
+        twitter_pattern = r"(https?://)(www\.)?(twitter\.com|x\.com)"
+        reddit_pattern = r"(https?://)(www\.)?(reddit\.com)"
+        tiktok_pattern = r"(https?://)(www\.)?((vm\.)?tiktok\.com)"
+        instagram_pattern = r"(https?://)(www\.)?(instagram\.com)"
         replacements = {
-            'twitter.com': 'fxtwitter.com',
-            'x.com': 'fixupx.com',
-            'reddit.com': 'rxddit.com',
-            'tiktok.com': 'tfxktok.com',
-            'vm.tiktok.com': 'tfxktok.com',
-            'instagram.com': 'kkinstagram.com'
+            "twitter.com": "fxtwitter.com",
+            "x.com": "fixupx.com",
+            "reddit.com": "rxddit.com",
+            "tiktok.com": "tfxktok.com",
+            "vm.tiktok.com": "tfxktok.com",
+            "instagram.com": "kkinstagram.com",
         }
 
         patterns = [twitter_pattern, reddit_pattern, tiktok_pattern, instagram_pattern]
@@ -35,12 +35,12 @@ class MessageCommands(discord.Cog, name="message_commands"):
             if match:
                 new_url = message.content
                 for old, new in replacements.items():
-                    new_url = re.sub(rf'https?://(www\.)?{old}', rf'https://{new}', new_url)
+                    new_url = re.sub(rf"https?://(www\.)?{old}", rf"https://{new}", new_url)
                 await ctx.respond(new_url, ephemeral=True)
                 return
 
         await ctx.respond("No supported link found!", ephemeral=True)
 
 
-def setup(bot):
+def setup(bot: discord.Bot) -> None:
     bot.add_cog(MessageCommands(bot))
