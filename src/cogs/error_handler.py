143b393ca41000ce5a887254d191e06d25d0c0dc
Toby <therealtobes28@gmail.com>
2026-02-09 20:05:18 +0000

    **Hopefully** make ruff happy...?
    This might take a few commits to get right, don't judge me ok.

--- src/cogs/error_handler.py
+++ src/cogs/error_handler.py
@@ -1,5 +1,4 @@
-"""
-error_handler.py
+"""Error handler cog.
 
 A simple error handling cog using on_application_command_error and on_command_error events.
 This handles events for both application (slash) commands and text (prefixed) commands.
@@ -12,82 +11,59 @@ Cog written by tobezdev:
     Website: https://tobezdev.com
 """
 
-import random
+import secrets
 import string
 
 import discord
 from discord import errors
 from discord.ext import commands
 
+_ERROR_MESSAGES: tuple[tuple[type[BaseException], str], ...] = (
+    (commands.CommandNotFound, "That command doesn't exist."),
+    (commands.MissingRequiredArgument, "You forgot to include a required argument."),
+    (commands.BadArgument, "One or more arguments were invalid or in the wrong format."),
+    (commands.DisabledCommand, "That command is currently disabled."),
+    (commands.NoPrivateMessage, "This command can't be used in DMs."),
+    (commands.MissingPermissions, "You don't have the required permissions to use this command."),
+    (commands.BotMissingPermissions, "I don't have the required permissions to perform that action."),
+    (commands.CommandOnCooldown, "That command is on cooldown. Try again in a moment."),
+    (commands.MaxConcurrencyReached, "Too many people are using this command at once. Please try again later."),
+    (errors.ExtensionFailed, "An extension failed to load due to an internal error."),
+    (errors.ExtensionNotFound, "Couldn't find that extension."),
+    (errors.ExtensionAlreadyLoaded, "That extension is already loaded."),
+    (errors.ExtensionNotLoaded, "That extension isn't currently loaded."),
+    (commands.NotOwner, "Only the bot owner can use this command."),
+    (commands.CheckFailure, "You didn't pass a permission or condition check for this command."),
+    (commands.CommandInvokeError, "An unexpected error occurred while running that command."),
+    (discord.InteractionResponded, "This interaction has already been responded to."),
+    (discord.ApplicationCommandInvokeError, "Something went wrong while executing that application command.",),
+    (discord.CheckFailure, "You don't meet the requirements to run this interaction."),
+    (discord.Forbidden, "I don't have permission to do that."),
+    (discord.NotFound, "That resource couldn't be found."),
+    (discord.DiscordServerError, "Discord's servers had an internal error."),
+    (discord.HTTPException, "A request to Discord's API failed unexpectedly."),
+    (discord.ConnectionClosed, "The connection to Discord was unexpectedly closed."),
+    (discord.GatewayNotFound, "Couldn't connect to Discord's gateway."),
+    (discord.InvalidArgument, "One or more arguments provided were invalid."),
+    (discord.InvalidData, "Discord returned invalid or incomplete data."),
+    (discord.ClientException, "The bot encountered a misuse or internal setup issue."),
+    (discord.LoginFailure, "The bot token provided is invalid."),
+)
 
-def get_msg_for_exception_type(error) -> str:
+
+def genErrCode(length: int = 12) -> str:
+    alphabet = string.ascii_uppercase + string.ascii_lowercase + string.digits
+    return "".join(secrets.choice(alphabet) for _ in range(length))
+
+
+def getErrMsg(error: Exception) -> str:
     msg: str | None = None
-    errcode: str = "".join(
-        random.choices(
-            population=string.ascii_uppercase + string.ascii_lowercase + string.digits,
-            k=12,
-        )
-    )
+    errcode: str = genErrCode()
 
-    match error:
-        case commands.CommandNotFound():
-            msg = "That command doesn't exist."
-        case commands.MissingRequiredArgument():
-            msg = "You forgot to include a required argument."
-        case commands.BadArgument():
-            msg = "One or more arguments were invalid or in the wrong format."
-        case commands.DisabledCommand():
-            msg = "That command is currently disabled."
-        case commands.NoPrivateMessage():
-            msg = "This command can't be used in DMs."
-        case commands.MissingPermissions():
-            msg = "You don't have the required permissions to use this command."
-        case commands.BotMissingPermissions():
-            msg = "I don't have the required permissions to perform that action."
-        case commands.CommandOnCooldown():
-            msg = "That command is on cooldown. Try again in a moment."
-        case commands.MaxConcurrencyReached():
-            msg = "Too many people are using this command at once. Please try again later."
-        case errors.ExtensionFailed():
-            msg = "An extension failed to load due to an internal error."
-        case errors.ExtensionNotFound():
-            msg = "Couldn't find that extension."
-        case errors.ExtensionAlreadyLoaded():
-            msg = "That extension is already loaded."
-        case errors.ExtensionNotLoaded():
-            msg = "That extension isn't currently loaded."
-        case commands.NotOwner():
-            msg = "Only the bot owner can use this command."
-        case commands.CheckFailure():
-            msg = "You didn't pass a permission or condition check for this command."
-        case commands.CommandInvokeError():
-            msg = "An unexpected error occurred while running that command."
-        case discord.InteractionResponded():
-            msg = "This interaction has already been responded to."
-        case discord.ApplicationCommandInvokeError():
-            msg = "Something went wrong while executing that application command."
-        case discord.CheckFailure():
-            msg = "You don't meet the requirements to run this interaction."
-        case discord.Forbidden():
-            msg = "I don't have permission to do that."
-        case discord.NotFound():
-            msg = "That resource couldn't be found."
-        case discord.DiscordServerError():
-            msg = "Discord's servers had an internal error."
-        case discord.HTTPException():
-            msg = "A request to Discord's API failed unexpectedly."
-        case discord.ConnectionClosed():
-            msg = "The connection to Discord was unexpectedly closed."
-        case discord.GatewayNotFound():
-            msg = "Couldn't connect to Discord's gateway."
-        case discord.InvalidArgument():
-            msg = "One or more arguments provided were invalid."
-        case discord.InvalidData():
-            msg = "Discord returned invalid or incomplete data."
-        case discord.ClientException():
-            msg = "The bot encountered a misuse or internal setup issue."
-        case discord.LoginFailure():
-            msg = "The bot token provided is invalid."
+    for exc_type, exc_msg in _ERROR_MESSAGES:
+        if isinstance(error, exc_type):
+            msg = exc_msg
+            break
 
     if msg is None or not msg:
         msg = f"An unknown `{error.__class__.__name__}` error occurred while processing this command."
@@ -100,7 +76,7 @@ class ErrorHandler(discord.Cog, name="error_handler"):
     def __init__(self, bot: discord.Bot) -> None:
         self.bot = bot
 
-    # Listner for application (slash) commands
+    # Listener for application (slash) commands
     @discord.Cog.listener()
     async def on_application_command_error(self, interaction: discord.Interaction, exception: Exception) -> None:
         print(exception)
@@ -108,9 +84,9 @@ class ErrorHandler(discord.Cog, name="error_handler"):
         # https://docs.pycord.dev/en/v2.7.0/api/clients.html#discord.Bot.on_application_command_error
         # This can be removed if the user does not want to print errors and only wants to handle them in Discord.
 
-        error_message: str = get_msg_for_exception_type(exception)
+        error_message: str = getErrMsg(exception)
         await interaction.respond(error_message, ephemeral=True)
 
 
-def setup(bot: discord.Bot):
+def setup(bot: discord.Bot) -> None:
     bot.add_cog(ErrorHandler(bot))
