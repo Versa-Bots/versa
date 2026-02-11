220d56e1fff04bd10558fd3b7f5053ba0d6b4096
ToothyDev <chosenfate@gmx.de>
2026-02-11 01:16:12 +0100

    Add docstring and fix wrong docstyle convention

--- src/exceptions.py
+++ src/exceptions.py
@@ -7,4 +7,10 @@ class UnexpectedNoneError(RuntimeError):
     """
 
     def __init__(self, obj: object, attr: str) -> None:
+        """
+        Initialize the custom exception raised when an attribute of an object is unexpectedly None.
+
+        :param obj: The object containing the attribute that is None.
+        :param attr: The name of the attribute that is unexpectedly None.
+        """
         super().__init__(f"{obj.__class__.__name__}.{attr} is None unexpectedly")
